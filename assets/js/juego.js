ca66265c3f593c7ff890ed172bf64dde39c919fd
higueraDev <davidhigueralp@hotmail.com>
2021-12-15 21:02:10 -0700

    funcion valorCarta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -4,17 +4,15 @@ let tipos = ["C", "D", "H", "S"];
 
 let cartaLetras = ["A", "J", "Q", "K"];
 
-
 // Esta funcion ordena un arreglo de manera aleatoria y lo retorna
 
 const shuffle = (target) => {
     let arr = [];
 
     for (i = 0; target.length != arr.length; i++) {
-        
         let sort = Math.floor(Math.random() * (target.length - 0) + 0);
 
-        if (!arr.find(check => check == target[sort])) {
+        if (!arr.find((check) => check == target[sort])) {
             arr.push(target[sort]);
         }
     }
@@ -43,10 +41,21 @@ const crearDeck = () => {
 
 crearDeck();
 
-
 // funcion que toma una carta del arreglo deck y lo muestra al usuario
 
-const tomarCarta = () => deck[0] ? deck.pop() : console.error("No hay mas cartas en el deck");
+const tomarCarta = () =>
+    deck[0] ? deck.pop() : console.error("No hay mas cartas en el deck");
 
-deck = [];
-console.log(tomarCarta(),deck);
+const CARTA = tomarCarta();
+
+console.log(deck, CARTA);
+
+// funcion que determina el valor numerico de una carta
+
+const valorCarta = (carta) => {
+    valor = carta.substring(0, carta.length - 1);
+    return isNaN(valor) ? (valor === "A" ? 11 : 10) : valor * 1;
+};
+
+console.log(valorCarta(CARTA));
+
